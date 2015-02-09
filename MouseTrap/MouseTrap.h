d851dcfac013b427cbb4119b4e470a84b25c0f51
Nolan Carroll <nolandcarroll@gmail.com>
2015-02-08 20:56:09 -0500

    Initial Commit

---
+++ MouseTrap/MouseTrap.h
@@ -0,0 +1,19 @@
+//
+//  MouseTrap.h
+//  MouseTrap
+//
+//  Created by Nolan Carroll on 2/8/15.
+//  Copyright (c) 2015 Nolan Carroll. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MouseTrap.
+FOUNDATION_EXPORT double MouseTrapVersionNumber;
+
+//! Project version string for MouseTrap.
+FOUNDATION_EXPORT const unsigned char MouseTrapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MouseTrap/PublicHeader.h>
+
+
